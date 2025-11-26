2672d719445124ca1907741ac6b880fc73633c40
pragyanshnakoti-design <pragyanshnakoti@gmail.com>
2025-11-26 09:10:46 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -9,4 +10,25 @@ class OTPVerify(BaseModel):
 
 class Token(BaseModel):
     access_token: str
-    token_type: str = "bearer"
+    token_type: str
+
+class AppointmentCreate(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+    date: str
+    time: str
+    description: str
+
+class AppointmentOut(BaseModel):
+    id: int
+    name: str
+    email: str
+    phone: str
+    date: str
+    time: str
+    description: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
